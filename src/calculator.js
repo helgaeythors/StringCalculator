64b27565ab701f679c7cad9db4e19fce5aa0f11e
unknown <helga.ey@gmail.com>
2018-10-08 20:50:25 +0000

    add shows the negative numbers from the string when throwing the exception

--- src/calculator.js
+++ src/calculator.js
@@ -3,12 +3,25 @@ function add (numbers){
 	if(numbers == "") // empty string
 		return 0;
 
-	if(numbers.includes("-")){ // throw exception if any negative numbers
-		throw "Negatives not allowed";
+	if(numbers.includes("-")){ // throw exception if there is a negative number
+		var negNumbers = numbers.match(/-\d+/g).map(Number);
+
+		var errorMessage = "Negatives not allowed: ";
+
+		for(var i = 0; i < numbers.length; i++){
+			errorMessage += negNumbers[i];
+
+			if(i != numbers.length - 1){
+				errorMessage += ",";
+			}
+		}
+
+		throw errorMessage;
 	}
 
 	if(numbers.includes("," || "\n")){ // multiple numbers and two delimeters
 		var numberArr = numbers.split(/[,\n]/);
+
 		var total = 0;
 		for(var i = 0; i < numberArr.length; i++){
 			total += parseInt(numberArr[i]);
